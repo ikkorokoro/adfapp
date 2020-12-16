7dd03d57dfc42c7bd1afc24efb59b2c07f3bac8c
今吉恒介 <>
2020-12-16 16:57:38 +0900

    不要なファイル、コード削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,19 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import '../css/tailwindcss.css';
-
+import $ from 'jquery'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-const autoprefixer = require('autoprefixer')
-const postcss = require('postcss')
-
-postcss([ autoprefixer ]).process(css).then(result => {
-  result.warnings().forEach(warn => {
-    console.warn(warn.toString())
-  })
-  console.log(result.css)
-})
